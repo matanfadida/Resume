da9a962f009e2fb76c57abf4b4163a21349b32bd
matanfadida <73058061+matanfadida@users.noreply.github.com>
2022-07-14 14:30:37 +0300

    navigation

---
+++ src/components/navigation/main-navigation.js
@@ -0,0 +1,45 @@
+import style from "./main-navigation.module.css";
+
+const MainNavigation = () => {
+  return (
+    <header>
+      <nav className={style.nav}>
+        <h2>Fadida Matan</h2>
+        <ul  className={style.ul}>
+          <li>
+            <a href="#top" className={style.li}>
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#About" className={style.li}>
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#Skills" className={style.li}>
+              Skills
+            </a>
+          </li>
+          <li>
+            <a href="#Sevices" className={style.li}>
+              Services
+            </a>
+          </li>
+          <li>
+            <a href="#Portfolio" className={style.li}>
+              Portfolio
+            </a>
+          </li>
+          <li>
+            <a href="#Contact" className={style.li}>
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainNavigation;
