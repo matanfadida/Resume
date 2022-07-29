f1efb51b1cc69b82ecd3ad00a1f314edca96d895
matanfadida <73058061+matanfadida@users.noreply.github.com>
2022-07-29 18:47:23 +0300

    fix

--- fronted/src/components/navigation/main-navigation.js
+++ fronted/src/components/navigation/main-navigation.js
@@ -1,37 +1,41 @@
 import { useContext, useEffect, useState } from "react";
 import style from "./main-navigation.module.css";
 // import { UilAlignJustify } from '@iconscout/react-unicons';
-import { UilMoon } from '@iconscout/react-unicons';
-import { UilSun } from '@iconscout/react-unicons';
+import { UilMoon } from "@iconscout/react-unicons";
+import { UilSun } from "@iconscout/react-unicons";
 import { ThemeContext } from "../../App";
 
 const MainNavigation = () => {
-
   const conTheme = useContext(ThemeContext);
   const [navbar, setNavbar] = useState(true);
-  const theme = conTheme.theme === 'dark';
+  const theme = conTheme.theme === "dark";
   // const [showIconMain, setShowIconMain] = useState(false);
-  console.log(conTheme)
 
   const changeBackground = () => {
-    if(window.scrollY > 0){
+    if (window.scrollY > 0) {
       setNavbar(false);
-    }
-    else{
+    } else {
       setNavbar(true);
     }
-  }
+  };
 
   useEffect(() => {
     changeBackground();
-    window.addEventListener("scroll",changeBackground);
-  })
+    window.addEventListener("scroll", changeBackground);
+  });
 
   return (
     <header>
-      <nav className={navbar ? style.nav : style["nav-move"]} style={theme ? {backgroundColor:'rgb(18, 17, 17)', color:"darkorange"} : null}>
+      <nav
+        className={navbar ? style.nav : style["nav-move"]}
+        style={
+          theme
+            ? { backgroundColor: "rgb(18, 17, 17)", color: "darkorange" }
+            : null
+        }
+      >
         <h2 className={style.h2}>Fadida Matan</h2>
-        <ul  className={style.ul}>
+        <ul className={style.ul}>
           <li className={style.li}>
             <a href="#top" className={theme ? style["a-dark"] : style.a}>
               Home
@@ -52,7 +56,15 @@ const MainNavigation = () => {
               Contact
             </a>
           </li>
-          {theme ? <i className={style.icon}><UilSun onClick={conTheme.toggleThemeHandler}/></i> : <i className={style.icon}><UilMoon onClick={conTheme.toggleThemeHandler}/></i>}
+          {theme ? (
+            <i className={style.icon}>
+              <UilSun onClick={conTheme.toggleThemeHandler} />
+            </i>
+          ) : (
+            <i className={style.icon}>
+              <UilMoon onClick={conTheme.toggleThemeHandler} />
+            </i>
+          )}
         </ul>
       </nav>
     </header>
